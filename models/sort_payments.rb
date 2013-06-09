15788019d23b0e92abb86d7f8591f46def75bdd9
linaresmariano <marianolinares92@gmail.com>
2013-06-09 16:25:14 -0300

    #4 el sort se hace en la base

--- models/sort_payments.rb
+++ models/sort_payments.rb
@@ -1,6 +1,9 @@
 class SortPayments
   def self.getLast(cant, account_id)
-    payments = Transaction.all(:account_id => account_id).all(:is_payment => true)
-    return (payments.sort! { |a,b| a.expiry_date <=> b.expiry_date })[0..cant-1]
+    payments = Transaction.all(:account_id => account_id)
+      .all(:is_payment => true)
+      .all(:order => [ :expiry_date.asc ])
+      
+    return payments[0..cant-1]
   end
 end
