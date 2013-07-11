dc50f0d6c64b7417be83d73b78f555147fa97a7a
linaresmariano <marianolinares92@gmail.com>
2013-07-11 03:03:28 -0300

    id del transaction... porque nos cambiaron estoooo???

--- db/migrate/003_create_transaction_dones.rb
+++ db/migrate/003_create_transaction_dones.rb
@@ -7,7 +7,7 @@ migration 3, :create_transaction_dones do
       column :pay_date, DateTime
       column :description, String, :length => 255
       column :is_payment, "BOOLEAN"
-      #column :transaction_id, Integer
+      column :transaction_id, Integer
       column :account_id, Integer
     end
   end
